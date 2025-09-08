a9ccc8cc31387325049c657862e56356923a02f1
yerim Jeong <dnaer456@gmail.com>
2025-09-08 21:22:11 +0900

    C# 기본 프로그래밍 + 기본 연산자

---
+++ chapter1.cs
@@ -0,0 +1,60 @@
+namespace FirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            statement: 문장은 프로그래밍에서 실행할 수 있는 최소 단위의 코드
+
+            키워드: 특별한 의미가 부여된 단어
+
+            식별자: 이름을 붙일 때에 사용하는 단어
+            (특수 문자는 _ 만 허용, 숫자로 사용X, 키워드 사용X, 공백 입력X)
+            클래스, 속성, 메소드, 네임스페이스 이름은 항상 `대문자`로 시작
+            지역, 전역 변수 이름 항상 `소문자`로 시작
+            여러 단어로 이루어진 식별자는 각 단어의 첫 글자를 `대문자`로 함
+            Ex) 
+            i love you -> iLoveYou , i am a boy -> iAmABoy
+
+            메소드: C#에서 식별자 뒤에 괄호가 있으면 메소드라고 함 이 이외의 것은 변수
+
+            주석: 코드 설명
+            Ex) 한 줄 : // , 여러 줄 : /* /
+
+            매개변수(파라미터): 메소드 괄호 안에 넣는 것
+            Ex) Floor(10,1)
+
+            규칙 지켜야 `유지보수` 가능 
+
+             */
+
+
+            // 프로그래밍 첫걸음
+            // 1-1
+            Console.Write("Write");
+            Console.Write("Write");
+            Console.Write("Write");
+            Console.Write("Write");
+
+            // 1-2
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+
+            // 1-3
+            Console.Write("Write");
+            Console.WriteLine("WriteLine");
+
+            Console.WriteLine("WriteLine");
+            Console.WriteLine("WriteLine");
+
+            Console.Write("Write");
+            Console.Write("Write");
+
+           
+        }
+    }
+}
---
+++ chapter2.cs
@@ -0,0 +1,106 @@
+namespace Chapter2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 기본 문법
+            // 2-1
+            Console.Write("Hello C# Programming .. !");
+
+            /*
+             연산자  설명 
+               +   더하기
+               -   빼기
+               *   곱하기
+               /   나누기
+               %   나머지
+             */
+
+            // 2-2
+            Console.WriteLine(52);
+
+            // 2-3
+            Console.WriteLine(52 + 273);
+
+            // 2-4
+            // 결과 예측
+            Console.WriteLine(5 + 3 * 2);
+
+            // 2-5
+            Console.WriteLine(10 % 5);
+            Console.WriteLine(7 % 3);
+
+            // 2-6
+            Console.WriteLine(1 + 2);
+            Console.WriteLine(1 - 2);
+            Console.WriteLine(1 * 2);
+            Console.WriteLine(1 / 2);
+            Console.WriteLine(1 % 2);
+
+            // 2-7 ( 왼쪽 피연산자의 부호 따름 )
+            Console.WriteLine(4 % 3);
+            Console.WriteLine(-4 % 3);
+            Console.WriteLine(4 % -3);
+            Console.WriteLine(-4 % -3);
+
+            // 2-8 ( 실수 )
+            Console.WriteLine(52.273);
+
+            // 2-9 ( 정수와 실수 )
+            Console.WriteLine(0); // 정수 
+            Console.WriteLine(0.0); // 실수
+
+            // 2-10 (실수와 사칙연산 )
+            Console.WriteLine(1.0 + 2.0);
+            Console.WriteLine(1.0 - 2.0);
+            Console.WriteLine(1.0 * 2.0);
+            Console.WriteLine(1.0 / 2.0);
+
+            // 2-11
+            Console.WriteLine(5.0 % 2.2); // 결과 : 0.5999999999999996 
+            Console.WriteLine(Math.Round(5.0 % 2.2, 2)); // 결과 : 0.6
+
+            // 2-12
+            Console.WriteLine("A");
+            Console.WriteLine("가");
+            Console.WriteLine("나");
+
+            // 2-13
+            Console.WriteLine("안녕하세요");
+
+            // 2-14
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+
+            // 2-15
+            Console.WriteLine("가나다" + "라마" + "바사아" + "자차카타" + "파하");
+
+            // 2-16
+            Console.WriteLine("안녕하세요"[0]);
+            Console.WriteLine("안녕하세요"[1]);
+            Console.WriteLine("안녕하세요"[3]);
+
+            // 2-17
+            // Console.WriteLine("안녕하세요"[100]);
+
+            // 2-18
+            Console.WriteLine('가' + '힣');
+
+            // 2-19
+            Console.WriteLine(true); // 참
+            Console.WriteLine(false); // 거짓
+
+            // 2-20
+            Console.WriteLine(52 < 273);
+            Console.WriteLine(52 > 273);
+
+            // 2-21
+            Console.WriteLine(!true);
+            Console.WriteLine(!false);
+            Console.WriteLine(!(52 < 273));
+            Console.WriteLine(! (52 > 273));
+        }
+    }
+}
