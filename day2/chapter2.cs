6b4e5c9e1f0f2d2993dc3c58ffaef91538d5ff7b
yerim Jeong <dnaer456@gmail.com>
2025-10-09 16:07:22 +0900

    문제

--- day2/chapter2.cs
+++ day2/chapter2.cs
@@ -6,3 +6,20 @@ for (int i = 0; i < numbers.Length; i++)
 }
 
 // 답 : 1, 2, 3
+
+using System;
+
+namespace chapter2
+{
+ class Program
+ {
+  static void Main(string[] args)
+  {
+   //HINT 1inch = 2.54cm 
+
+   Console.WriteLine("inch 단위를 입력하세요 : ");
+   double inch = double.Parse(Console.ReadLine());
+   Console.WriteLine(inch + "inch는 " + (inch * 2.54) + "cm입니다.");
+  }
+ }
+}
