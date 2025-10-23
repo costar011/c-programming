e2067bb40748a3b868e91717c6a3f3e835a3d59a
yerim Jeong <dnaer456@gmail.com>
2025-10-23 10:02:10 +0900

    해의 띠 출력 프로그램

--- example/1.cs
+++ example/1.cs
@@ -36,3 +36,60 @@ else
 {
  Console.WriteLine("미성년자");
 }
+
+
+//주어진 정수가 3의 배수인 경우 “3의 배수입니다.”를 출력하고, 그렇지 않은 경우 “3의 배수가 아닙니다.”를 출력하는 코드를 작성하시오.
+Console.Write("정수를 입력하세요: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 3 == 0)
+{
+ Console.WriteLine("3의 배수입니다.");
+}
+else
+{
+ Console.WriteLine("3의 배수가 아닙니다.");
+}
+
+// 사용자에게 태어난 연도를 입력 받아 그 해의 띠를 출력하는 프로그램을 작성하시오.
+Console.Write("태어난 연도: ");
+int birth = int.Parse(Console.ReadLine());
+switch (birth % 12)
+{
+ case 0:
+  Console.WriteLine("원숭이 띠입니다.");
+  break;
+ case 1:
+  Console.WriteLine("닭 띠입니다.");
+  break;
+ case 2:
+  Console.WriteLine("개 띠입니다.");
+  break;
+ case 3:
+  Console.WriteLine("돼지 띠입니다.");
+  break;
+ case 4:
+  Console.WriteLine("쥐 띠입니다.");
+  break;
+ case 5:
+  Console.WriteLine("소 띠입니다.");
+  break;
+ case 6:
+  Console.WriteLine("호랑이 띠입니다.");
+  break;
+ case 7:
+  Console.WriteLine("토끼 띠입니다.");
+  break;
+ case 8:
+  Console.WriteLine("용 띠입니다.");
+  break;
+ case 9:
+  Console.WriteLine("뱀 띠입니다.");
+  break;
+ case 10:
+  Console.WriteLine("말 띠입니다.");
+  break;
+ case 11:
+  Console.WriteLine("양 띠입니다.");
+  break;
+}
