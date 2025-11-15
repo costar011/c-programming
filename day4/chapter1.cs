ed25f887c078f92a40b550ae269351189a5f36fd
yerim Jeong <dnaer456@gmail.com>
2025-11-15 10:43:50 +0900

    구조체

---
+++ day4/chapter1.cs
@@ -0,0 +1,52 @@
+// 구조체 값 복사
+
+class Program
+{
+ class PointClass
+ {
+  public int x;
+  public int y;
+
+  public PointClass(int x, int y)
+  {
+   this.x = x;
+   this.y = y;
+  }
+ }
+
+ struct PointStruct
+ {
+  public int x;
+  public int y;
+
+  public PointStruct(int x, int y)
+  {
+   this.x = x;
+   this.y = y;
+  }
+ }
+
+ static void Main(string[] args)
+ {
+  // 클래스
+  PointClass pointClassA = new PointClass(10, 20);
+  PointClass pointClassB = pointClassA;   // 참조 복사가 일어납니다.
+
+  pointClassB.x = 100;
+  pointClassB.y = 200;
+
+  Console.WriteLine("pointClassA: " + pointClassA.x + "," + pointClassA.y);
+  Console.WriteLine("pointClassB: " + pointClassB.x + "," + pointClassB.y);
+  Console.WriteLine();
+
+  // 구조체
+  PointStruct pointStructA = new PointStruct(10, 20);
+  PointStruct pointStructB = pointStructA;  // 값 복사가 일어납니다.
+
+  pointStructB.x = 100;
+  pointStructB.y = 200;
+
+  Console.WriteLine("pointStructA: " + pointStructA.x + "," + pointStructA.y);
+  Console.WriteLine("pointStructB: " + pointStructB.x + "," + pointStructB.y);
+ }
+}
